05aced71476fe1455d935691f7eae4a9bfc779c3
karan kose <karankose.ntf@gmail.com>
2025-06-18 18:32:22 +0530

    cros fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // app.use(cors());
 app.use(cors({
-  origin: ['http://localhost:8080', 'https://devinfotech.site/'],
+  origin: ['http://localhost:8080', 'https://devinfotech.site'],
   credentials: true,
 }));
 app.use(express.json());
