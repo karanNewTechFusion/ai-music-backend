9041b854a109ec14bcb49997d9c3e145a60077b5
karan kose <karankose.ntf@gmail.com>
2025-06-20 17:12:22 +0530

    fixed cors

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 // app.use(cors());
 app.use(cors({
-  origin: ['http://localhost:8080', 'https://devinfotech.site'],
-  credentials: true,
+  origin: (origin, callback) => {
+    callback(null, true); // Allow all origins
+  },
+  credentials: true, // Allow cookies/auth headers
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
