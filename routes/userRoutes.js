4332e5ac85a1a81d5ce2d4a470651b5c3c42aef0
karan kose <karankose.ntf@gmail.com>
2025-06-25 10:45:51 +0530

    backend audio api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import { signUpUser, loginUser } from '../controllers/userController.js';
+import express from "express";
+import { signUpUser, loginUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post('/signup', signUpUser);
-router.post('/login', loginUser);
+router.post("/signup", signUpUser);
+router.post("/login", loginUser);
 
 export default router;
